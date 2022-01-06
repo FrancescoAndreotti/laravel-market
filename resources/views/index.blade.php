242ffca6a537169700678ac11a2f4aa06b66c5e9
FrancescoAndreotti <francescoandreotti00@gmail.com>
2022-01-06 12:39:19 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @include('partials.link')
+  <title>Index</title>
+</head>
+<body>
+  @include('partials.the_header')
+</body>
+</html>
